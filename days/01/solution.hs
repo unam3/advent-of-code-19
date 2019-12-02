f20eeb0cc3fd63c29f1b35396989a5cfa75ce241
Flowers Safety <40meta@gmail.com>
2019-12-03 01:07:23 +0600

    day 1 p2 solution

--- days/01/solution.hs
+++ days/01/solution.hs
@@ -8,10 +8,27 @@ parseInput :: String -> [Mass]
 parseInput = map read . lines
 
 fuelRequired :: Mass -> Fuel
-fuelRequired x = floor (realToFrac x / 3) - 2
+fuelRequired mass = floor (realToFrac mass / 3) - 2
 
 getRequiredFuelForMassess :: [Mass] -> Fuel
 getRequiredFuelForMassess = List.foldl' (\acc x -> acc + fuelRequired x) 0 
+
+totalFuelRequired' :: Mass -> Fuel -> Fuel
+totalFuelRequired' mass accFuel =
+    let {
+        computedFuel = floor (realToFrac mass / 3) - 2;
+    } in if computedFuel > 0
+        then totalFuelRequired' computedFuel accFuel + computedFuel
+        else accFuel
+
+totalFuelRequired :: Mass -> Fuel
+totalFuelRequired mass =
+    let {
+        accFuelInit = 0;
+    } in totalFuelRequired' mass accFuelInit
+
+getTotalRequiredFuelForMassess :: [Mass] -> Fuel
+getTotalRequiredFuelForMassess = List.foldl' (\acc x -> acc + totalFuelRequired x) 0 
 
 interactWith :: (String -> String) -> FilePath -> FilePath -> IO ()
 interactWith f inputFile outputFile = do
@@ -26,8 +43,10 @@ main = mainWith solvePuzzle
                 [input, output] -> interactWith f input output
                 _ -> putStrLn "error: exactly two arguments needed"
 
-          solvePuzzle input = 
-              "First part solution is: " ++ firstPuzzlePart where
-                firstPuzzlePart = let {
-                        modulesMassList = parseInput input;
-                    } in show $ getRequiredFuelForMassess modulesMassList
+          solvePuzzle input =
+            let {
+                modulesMassList = parseInput input;
+                firstPuzzlePart = show $ getRequiredFuelForMassess modulesMassList;
+                secondPuzzlePart = show $ getTotalRequiredFuelForMassess modulesMassList;
+            } in "First part solution is: " ++ firstPuzzlePart
+               ++ "\n" ++ "Second part solution is: " ++ secondPuzzlePart
